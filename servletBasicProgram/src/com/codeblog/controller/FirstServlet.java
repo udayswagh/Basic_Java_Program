59bab1eeb40daf09d5bd65f56f1c0c9dcdbbd42e
udayswagh <54849530+udayswagh@users.noreply.github.com>
2019-12-09 11:01:49 +0530

    Add files via upload

---
+++ servletBasicProgram/src/com/codeblog/controller/FirstServlet.java
@@ -0,0 +1,48 @@
+package com.codeblog.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class FirstServlet
+ */
+@WebServlet("/FirstServlet")
+public class FirstServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter out=response.getWriter();
+		
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title> First servlet welcome</title>");
+		out.println("</head>");
+		out.println("<body>");
+		
+		out.println( new Date().toString());
+		
+		out.println("<hr>");
+		out.println("</body>");
+		out.println("</html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
